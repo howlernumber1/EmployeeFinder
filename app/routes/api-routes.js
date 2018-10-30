b76a41b40c7f58256e116fb4124469a854663823
markj06 <mjjohnson06@gmail.com>
2018-10-30 14:33:51 -0400

    updateing js files

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -2,7 +2,7 @@
 // DEPENDENCIES
 // We need to include the path package to get the correct file path for our html
 // ===============================================================================
-const path = require('path');
+let employeeList = require('../data/employees.js');
 
 
 // ===============================================================================
@@ -14,12 +14,7 @@ module.exports = function(app) {
   // Below code handles when users 'visit' a page.
   // ---------------------------------------------------------------------------
 
-  // If no matching route is found default to home
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/home.html'));
+  app.get('/api/employees', function(req, res) {
+    res.json(employeeList)
   });
 };
-
-  app.get('/survey', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/survey.html'));
-  });
