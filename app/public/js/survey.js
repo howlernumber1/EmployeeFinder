abc66d81cd171239bc535f8c5185c66bad0bc123
markj06 <mjjohnson06@gmail.com>
2018-11-01 08:12:07 -0400

    adding logic to js files and modified survey.html

--- app/public/js/survey.js
+++ app/public/js/survey.js
@@ -1,1 +1,42 @@
+$(function() {
 
+  // Render function to display match to modal
+  const render = function(match) {
+
+    // append result match to modal
+    $('#result-name').text(match.name);
+    $('#result-img').attr('src', match.photo)
+
+    //toggle modal on
+    $('#seeResult').modal('toggle');
+  }
+
+  //function to capture input values
+  const surveyEntry = function(event) {
+    event.preventDefault();
+
+    //get inputs
+    const surveyInfo = {
+      name: $('#inputName').val().trim(),
+      photo: $('#inputImg').val().trim(),
+      scores: [
+        $('select#quest1').val().trim(),
+        $('select#quest2').val().trim(),
+        $('select#quest3').val().trim(),
+        $('select#quest4').val().trim(),
+        $('select#quest5').val().trim(),
+        $('select#quest6').val().trim(),
+        $('select#quest7').val().trim(),
+        $('select#quest8').val().trim(),
+        $('select#quest9').val().trim(),
+        $('select#quest10').val().trim()
+      ]
+    }
+
+    //Post stored values into array
+    $.post('api/employees', surveyInfo, render)
+  }
+
+  $('#submitSurvey').on('click', surveyEntry)
+
+});
--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -16,5 +16,12 @@ module.exports = function(app) {
 
   app.get('/api/employees', function(req, res) {
     res.json(employeeList)
+    // console.log(json.stringify(employeeList));
   });
+
+  app.post('/api/employees', function(req, res) {
+
+  employeeList.push(req.body);
+  res.end();
+  })
 };
