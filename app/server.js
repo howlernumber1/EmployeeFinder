01549e8a7eb3e4e2590d904ee575d26a94eaf95b
markj06 <mjjohnson06@gmail.com>
2018-11-02 21:45:55 -0400

    changed server port

--- app/server.js
+++ app/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Defines a PORT for the server to listen for requests
-const PORT = 8080;
+const PORT = process.env.PORT || 3000;
 
 // Sets up our server to parse our request body for usage
 app.use(express.urlencoded({ extended: true }));
@@ -22,5 +22,5 @@ require('./routes/html-routes.js')(app);
 
 // Starts our server on the predefined PORT
 app.listen(PORT, function(){
-  console.log(`App is now listening on PORT ${PORT}`)
+  // console.log(`App is now listening on PORT ${PORT}`)
 })
